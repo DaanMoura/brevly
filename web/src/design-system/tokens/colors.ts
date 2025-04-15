420875af89f6aaae93a06f16254a21e49c9d0afc
Daniel Moura <daniel.lucio.moura@gmail.com>
2025-04-14 22:02:23 -0300

    add design system

---
+++ web/src/design-system/tokens/colors.ts
@@ -0,0 +1,14 @@
+const colors = {
+  blueBase: { value: '#2C46B1' },
+  blueDark: { value: '#2C4091' },
+  white: { value: '#FFFFFF' },
+  gray100: { value: '#F9F9FB' },
+  gray200: { value: '#E4E6EC' },
+  gray300: { value: '#CDCFD5' },
+  gray400: { value: '#74798B' },
+  gray500: { value: '#4D505C' },
+  gray600: { value: '#1F2025' },
+  danger: { value: '#B12C4D' }
+}
+
+export default colors
---
+++ web/src/design-system/tokens/typography.ts
@@ -0,0 +1,45 @@
+import { defineTextStyles } from '@pandacss/dev'
+
+export const textStyles = defineTextStyles({
+  textXl: {
+    value: {
+      fontFamily: '"Open Sans", sans-serif',
+      fontSize: '24px',
+      lineHeight: '32px',
+      fontWeight: 'bold'
+    }
+  },
+  textLg: {
+    value: {
+      fontFamily: '"Open Sans", sans-serif',
+      fontSize: '18px',
+      lineHeight: '24px',
+      fontWeight: 'bold'
+    }
+  },
+  textMd: {
+    value: {
+      fontFamily: '"Open Sans", sans-serif',
+      fontSize: '14px',
+      lineHeight: '18px',
+      fontWeight: 'semibold'
+    }
+  },
+  textSm: {
+    value: {
+      fontFamily: '"Open Sans", sans-serif',
+      fontSize: '12px',
+      lineHeight: '16px',
+      fontWeight: 'normal'
+    }
+  },
+  textXs: {
+    value: {
+      fontFamily: '"Open Sans", sans-serif',
+      fontSize: '10px',
+      lineHeight: '14px',
+      fontWeight: 'normal',
+      textTransform: 'uppercase'
+    }
+  }
+})
